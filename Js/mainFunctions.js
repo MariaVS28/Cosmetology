0954c3b9321f2f99a72a844150f773fc03a206bb
Maria Vasilieva <mariyavasilieva01@gmail.com>
2021-02-03 10:49:14 +0300

    Added signUp

---
+++ Js/mainFunctions.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("popup__background").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("popup__background").style.display = "none";
+  }
