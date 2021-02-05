e4dce6ccf80e3196871dab1bef404d27b1c2eee5
Maria Vasilieva <mariyavasilieva01@gmail.com>
2021-02-05 14:39:12 +0300

    Added popup

--- Js/mainFunctions.js
+++ Js/mainFunctions.js
@@ -1,9 +1,15 @@
-function openForm() {
+function openSignUp() {
     document.getElementById("myForm").style.display = "block";
     document.getElementById("popup__background").style.display = "block";
   }
   
-  function closeForm() {
+function openSupport() {
+    document.getElementById("myForm2").style.display = "block";
+    document.getElementById("popup__background").style.display = "block";
+  }
+  
+function closePopups() {
     document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm2").style.display = "none";
     document.getElementById("popup__background").style.display = "none";
   }
