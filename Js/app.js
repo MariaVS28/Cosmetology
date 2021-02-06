73742e8862a0b149173647b448f1bd367c5c5ee4
Maria Vasilieva <mariyavasilieva01@gmail.com>
2021-02-06 20:39:32 +0300

    renamed form ids

--- Js/app.js
+++ Js/app.js
@@ -1,21 +1,24 @@
  function openSignUp() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById("signupForm").style.display = "block";
     document.getElementById("popup__background").style.display = "block";
   }
   
 function openSupport() {
-    document.getElementById("myForm2").style.display = "block";
+    document.getElementById("supportForm").style.display = "block";
     document.getElementById("popup__background").style.display = "block";
   }
   
 function closePopups() {
-    document.getElementById("myForm").style.display = "none";
-    document.getElementById("myForm2").style.display = "none";
+    document.getElementById("signupForm").style.display = "none";
+    document.getElementById("supportForm").style.display = "none";
     document.getElementById("popup__background").style.display = "none";
   }
 
 $(function() {
 
-  
+  let popupBackground = $("#popup__background");
+  let signupForm = $("#signupForm");
+  let supportForm = $("#supportForm");
 
+  //popupBackground.on("click", closePopups);
 });
